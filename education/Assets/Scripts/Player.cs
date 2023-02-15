ecce2e34dae912f41ebe7f8c4a79f03815d36678
Alecsandr-Medvedev <92380319+Alecsandr-Medvedev@users.noreply.github.com>
2023-02-15 18:57:03 +0700

    Л2 Задача №2

--- education/Assets/Scripts/Player.cs
+++ education/Assets/Scripts/Player.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
+    [SerializeField] int _power;
+
     private HingeJoint2D joint;
+    private Rigidbody2D rig;
    
     void Start()
     {
         joint = GetComponent<HingeJoint2D>();
+        rig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -18,6 +22,14 @@ public class Player : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             joint.enabled = false;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rig.AddRelativeForce(new Vector2(-_power, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rig.AddRelativeForce(new Vector2(_power, 0));
         }
     }
 }
